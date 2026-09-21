2d705c3b909f326ddb36f764fad38d3c1f74f1da
Vishal K Setti <vishalsetti2000@gmail.com>
2026-09-21 19:23:05 -0400

    ui: action bar above textarea so Send stays visible with keyboard open

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "typebridge-v2";
+const CACHE = "typebridge-v3";
 const FILES = ["/", "/manifest.json", "/icon.png"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener("fetch", e => {
